dbcf876a6de291b1cf1d251f8c2037df8c114749
ametreniuc <metreniuk@gmail.com>
2018-11-11 01:02:00 +0200

    Add game-of-life module

---
+++ src/modules/game-of-life/__test__/main.test.js
@@ -0,0 +1,302 @@
+let {
+  makeWorld,
+  makeRandomWorld,
+  neighborsSum,
+  cellNextState,
+  worldNextState,
+} = require("../index")
+
+describe("makeWorld", () => {
+  test("generates a bi-dimensional array filled with zeroes", () => {
+    expect(makeWorld(2, 3)).toEqual([[0, 0, 0], [0, 0, 0]])
+  })
+})
+
+describe("makeRandomWorld", () => {
+  test("generates a bi-dimensional array", () => {
+    let randomWorld = makeRandomWorld(2, 3)
+    expect(randomWorld).toHaveLength(2)
+    randomWorld.forEach(row => expect(row).toHaveLength(3))
+  })
+})
+
+describe("neighborsSum", () => {
+  test("adds all 8 neighboors", () => {
+    let x = 1,
+      y = 1,
+      world = [
+        //
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 0, 0],
+      ],
+      margins = [-1, world.length]
+
+    expect(neighborsSum(x, y, world, margins)).toBe(3)
+  })
+
+  test("excludes the current cell", () => {
+    let x = 1,
+      y = 1,
+      world = [
+        //
+        [0, 1, 0],
+        [0, 1, 1],
+        [1, 0, 0],
+      ],
+      margins = [-1, world.length]
+
+    expect(neighborsSum(x, y, world, margins)).toBe(3)
+  })
+
+  test("handles the zero margin", () => {
+    let x = 0,
+      y = 0,
+      world = [
+        //
+        [0, 1, 0],
+        [0, 1, 1],
+        [1, 0, 0],
+      ],
+      margins = [-1, world.length]
+
+    expect(neighborsSum(x, y, world, margins)).toBe(2)
+  })
+
+  test("handles the max length margin", () => {
+    let world = [
+        //
+        [0, 1, 0],
+        [0, 1, 1],
+        [1, 1, 0],
+      ],
+      x = world.length - 1,
+      y = world.length - 1,
+      margins = [-1, world.length]
+
+    expect(neighborsSum(x, y, world, margins)).toBe(3)
+  })
+})
+
+// TODO handle false positives
+describe("cellNextState", () => {
+  test("any live cell with fewer than two live neighbors DIES, as if by underpopulation", () => {
+    let world = [
+        //
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 0, 0],
+      ],
+      x = 1,
+      y = 1,
+      margins = [-1, world.length]
+
+    expect(cellNextState(world, margins)(x, y)).toBe(0)
+  })
+
+  test("any live cell with two or three live neighbors LIVES on to the next generation", () => {
+    let world = [
+        //
+        [0, 1, 1],
+        [0, 1, 0],
+        [0, 0, 0],
+      ],
+      x = 1,
+      y = 1,
+      margins = [-1, world.length]
+
+    expect(cellNextState(world, margins)(x, y)).toBe(1)
+
+    world = [
+      //
+      [0, 1, 1],
+      [0, 1, 1],
+      [0, 0, 0],
+    ]
+    expect(cellNextState(world, margins)(x, y)).toBe(1)
+  })
+
+  test("any live cell with more than three live neighbors DIES, as if by overpopulation", () => {
+    let world = [
+        //
+        [0, 1, 1],
+        [0, 1, 1],
+        [0, 0, 1],
+      ],
+      x = 1,
+      y = 1,
+      margins = [-1, world.length]
+
+    expect(cellNextState(world, margins)(x, y)).toBe(0)
+  })
+
+  test("any dead cell with exactly three live neighbors becomes a LIVE cell, as if by reproduction", () => {
+    let world = [
+        //
+        [0, 1, 1],
+        [0, 0, 1],
+        [0, 0, 0],
+      ],
+      x = 1,
+      y = 1,
+      margins = [-1, world.length]
+
+    expect(cellNextState(world, margins)(x, y)).toBe(1)
+  })
+
+  test("random", () => {
+    let world = [
+        //
+        [1, 1, 0],
+        [1, 0, 0],
+        [0, 0, 0],
+      ],
+      x = 1,
+      y = 1,
+      margins = [-1, world.length]
+
+    expect(cellNextState(world, margins)(x, y)).toBe(1)
+  })
+})
+
+describe("worldNextState", () => {
+  test("kinda works", () => {
+    let samples = [
+      {
+        prev: [
+          //
+          [0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0],
+        ],
+        next: [
+          //
+          [0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0],
+        ],
+      },
+      {
+        prev: [
+          //
+          [1, 1, 0],
+          [1, 0, 0],
+          [0, 0, 0],
+        ],
+        next: [
+          //
+          [1, 1, 0],
+          [1, 1, 0],
+          [0, 0, 0],
+        ],
+      },
+      {
+        prev: [
+          //
+          [0, 1, 0],
+          [1, 0, 1],
+          [0, 0, 0],
+        ],
+        next: [
+          //
+          [0, 1, 0],
+          [0, 1, 0],
+          [0, 0, 0],
+        ],
+      },
+      {
+        prev: [
+          //
+          [0, 1, 0],
+          [0, 1, 0],
+          [0, 0, 1],
+        ],
+        next: [
+          //
+          [0, 0, 0],
+          [0, 1, 1],
+          [0, 0, 0],
+        ],
+      },
+      {
+        prev: [
+          //
+          [0, 0, 1],
+          [0, 1, 0],
+          [1, 0, 0],
+        ],
+        next: [
+          //
+          [0, 0, 0],
+          [0, 1, 0],
+          [0, 0, 0],
+        ],
+      },
+      {
+        prev: [
+          //
+          [1, 1, 0],
+          [1, 0, 0],
+          [0, 0, 0],
+        ],
+        next: [
+          //
+          [1, 1, 0],
+          [1, 1, 0],
+          [0, 0, 0],
+        ],
+      },
+      {
+        // blinker
+        prev: [
+          //
+          [0, 0, 0],
+          [1, 1, 1],
+          [0, 0, 0],
+        ],
+        next: [
+          //
+          [0, 1, 0],
+          [0, 1, 0],
+          [0, 1, 0],
+        ],
+      },
+      {
+        // block
+        prev: [
+          //
+          [0, 1, 1],
+          [0, 1, 1],
+          [0, 0, 0],
+        ],
+        next: [
+          //
+          [0, 1, 1],
+          [0, 1, 1],
+          [0, 0, 0],
+        ],
+      },
+      {
+        // glider 0
+        prev: [
+          //
+          [0, 1, 0, 0],
+          [0, 0, 1, 0],
+          [1, 1, 1, 0],
+          [0, 0, 0, 0],
+        ],
+        next: [
+          //
+          [0, 0, 0, 0],
+          [1, 0, 1, 0],
+          [0, 1, 1, 0],
+          [0, 1, 0, 0],
+        ],
+      },
+    ]
+
+    samples.forEach(({ prev, next }) => {
+      expect(worldNextState(prev)).toEqual(next)
+    })
+  })
+})
---
+++ src/modules/game-of-life/index.js
@@ -0,0 +1,51 @@
+let makeWorld = (n, m) => new Array(n).fill(new Array(m).fill(0))
+
+let mapWorld = (fn, world) => world.map((row, x) => row.map((_, y) => fn(x, y)))
+
+let makeRandomWorld = (n, m) =>
+  mapWorld(() => (Math.random() > 0.5 ? 1 : 0), makeWorld(n, m))
+
+let neighborsSum = (x, y, world, margins) => {
+  let sum = 0
+  for (let i = x - 1; i <= x + 1; i++) {
+    if (margins.includes(i)) continue
+    for (let j = y - 1; j <= y + 1; j++) {
+      if ((i === x && j === y) || margins.includes(j)) {
+        continue
+      }
+      sum += world[i][j]
+    }
+  }
+  return sum
+}
+
+/**
+ * 
+ * 
+Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+Any live cell with two or three live neighbors lives on to the next generation.
+Any live cell with more than three live neighbors dies, as if by overpopulation.
+Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+ */
+
+let shouldDie = sum => sum < 2 || sum > 3
+let shouldBeBorn = sum => sum === 3
+
+let handleLive = sum => (shouldDie(sum) ? 0 : 1)
+let handleDead = sum => (shouldBeBorn(sum) ? 1 : 0)
+
+let cellNextState = (world, margins) => (x, y) =>
+  Boolean(world[x][y])
+    ? handleLive(neighborsSum(x, y, world, margins))
+    : handleDead(neighborsSum(x, y, world, margins))
+
+let worldNextState = (world, margins = [-1, world.length]) =>
+  mapWorld(cellNextState(world, margins), world)
+
+export {
+  makeWorld,
+  makeRandomWorld,
+  neighborsSum,
+  cellNextState,
+  worldNextState,
+}

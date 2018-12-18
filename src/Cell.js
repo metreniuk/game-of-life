6c05cf61303e9b3d81581b3a892df49e2727059c
ametreniuc <metreniuk@gmail.com>
2018-12-18 22:04:56 +0200

    Make the cell look awesome!

--- src/Cell.js
+++ src/Cell.js
@@ -1,34 +1,170 @@
 import React from "react"
 import styled from "styled-components"
 
-// export const Cell = () => (
-//   <div className="cell box alive">
-//     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-//       <circle cx="6" cy="8" r="2" />
-//       <circle cx="18" cy="8" r="2" />
-//     </svg>
-//   </div>
-// )
-
 let Box = styled.div`
-  border: 1px #006ec8 solid;
-  fill: #006ec8;
   height: 32px;
   width: 32px;
-  margin-right: 5px;
-  margin-bottom: 5px;
   cursor: pointer;
-  border-radius: 2px;
 
   background: ${props => (props.alive ? "#006ec8" : "#fff")};
+  fill: ${props => (props.alive ? "#fff" : "#006ec8")};
+  border: 1px solid #006ec8;
 `
-
-// export let Cell = () => {
-//   return (
-//     <Toggle>{({ on, toggle }) => <Box alive={on} onClick={toggle} />}</Toggle>
-//   )
-// }
-
+// TODO make animation when the cell disappears
 export let Cell = ({ alive, onClick }) => {
-  return <Box alive={alive} onClick={onClick} />
+  return (
+    <Box alive={alive} onClick={onClick}>
+      {alive ? <LivingSvg /> : <DeadSvg />}
+    </Box>
+  )
 }
+
+let LivingSvg = () => (
+  <svg x="0px" y="0px" viewBox="0 0 1000 1000">
+    <g>
+      {/* <path d="M933.33,66.67v866.67H66.67V66.67H933.33 M1000,0H0v1000h1000V0L1000,0z" /> */}
+      <g>
+        <g>
+          <g>
+            <g>
+              <rect x="687.59" y="283.52" width="49.91" height="49.91" />
+            </g>
+            <g>
+              <rect x="637.69" y="283.52" width="49.91" height="49.91" />
+            </g>
+            <g>
+              <rect x="687.59" y="333.43" width="49.91" height="49.91" />
+            </g>
+            <g>
+              <rect x="637.69" y="333.43" width="49.91" height="49.91" />
+            </g>
+          </g>
+          <g>
+            <g>
+              <rect x="311.3" y="283.52" width="49.91" height="49.91" />
+            </g>
+            <g>
+              <rect x="261.39" y="283.52" width="49.91" height="49.91" />
+            </g>
+            <g>
+              <rect x="311.3" y="333.43" width="49.91" height="49.91" />
+            </g>
+            <g>
+              <rect x="261.39" y="333.43" width="49.91" height="49.91" />
+            </g>
+          </g>
+        </g>
+        <g>
+          <rect x="674.12" y="585.28" width="49.91" height="49.91" />
+        </g>
+        <g>
+          <rect x="624.21" y="618.55" width="49.91" height="49.91" />
+        </g>
+        <g>
+          <rect x="324.77" y="618.55" width="49.91" height="49.91" />
+        </g>
+        <g>
+          <rect x="274.87" y="585.28" width="49.91" height="49.91" />
+        </g>
+        <g>
+          <g>
+            <rect x="574.3" y="668.46" width="49.91" height="49.91" />
+          </g>
+          <g>
+            <rect x="524.4" y="668.46" width="49.91" height="49.91" />
+          </g>
+          <g>
+            <rect x="474.49" y="668.46" width="49.91" height="49.91" />
+          </g>
+          <g>
+            <rect x="424.59" y="668.46" width="49.91" height="49.91" />
+          </g>
+          <g>
+            <rect x="374.68" y="668.46" width="49.91" height="49.91" />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+)
+
+let DeadSvg = () => (
+  <svg x="0px" y="0px" viewBox="0 0 1000 1000">
+    <g>
+      {/* <path d="M933.33,66.67v866.67H66.67V66.67H933.33 M1000,0H0v1000h1000V0L1000,0z" /> */}
+      <g>
+        <g>
+          <g>
+            <g>
+              <g>
+                <rect x="737.99" y="282.17" width="50" height="50" />
+              </g>
+              <g>
+                <rect x="637.99" y="382.17" width="50" height="50" />
+              </g>
+              <g>
+                <rect x="637.99" y="282.17" width="50" height="50" />
+              </g>
+              <g>
+                <rect x="687.99" y="332.17" width="50" height="50" />
+              </g>
+              <g>
+                <rect x="737.99" y="382.17" width="50" height="50" />
+              </g>
+            </g>
+          </g>
+          <g>
+            <g>
+              <g>
+                <rect x="312.01" y="282.17" width="50" height="50" />
+              </g>
+              <g>
+                <rect x="212.01" y="382.17" width="50" height="50" />
+              </g>
+              <g>
+                <rect x="212.01" y="282.17" width="50" height="50" />
+              </g>
+              <g>
+                <rect x="262.01" y="332.17" width="50" height="50" />
+              </g>
+              <g>
+                <rect x="312.01" y="382.17" width="50" height="50" />
+              </g>
+            </g>
+          </g>
+        </g>
+        <g>
+          <g>
+            <rect x="274.49" y="667.83" width="50" height="50" />
+          </g>
+          <g>
+            <rect x="324.49" y="642.83" width="50" height="50" />
+          </g>
+          <g>
+            <rect x="624.49" y="642.83" width="50" height="50" />
+          </g>
+          <g>
+            <rect x="674.49" y="667.83" width="50" height="50" />
+          </g>
+          <g>
+            <g>
+              <rect x="374.49" y="613.67" width="50" height="50" />
+            </g>
+            <g>
+              <rect x="424.49" y="613.67" width="50" height="50" />
+            </g>
+            <g>
+              <rect x="474.49" y="613.67" width="50" height="50" />
+            </g>
+            <g>
+              <rect x="524.49" y="613.67" width="50" height="50" />
+            </g>
+            <g>
+              <rect x="574.49" y="613.67" width="50" height="50" />
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+)

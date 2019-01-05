8612756b35a02c201ba8682f09858885fb25a2e0
ametreniuc <metreniuk@gmail.com>
2019-01-05 18:36:07 +0200

    Add slide about meeting the requirements

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ import theme from "mdx-deck/themes"
 export default {
   ...theme,
   font: "IBM Plex Mono, Overpass Mono, Helvetica, sans-serif",
-  fontSizes: ["16px", "16px", "32px"],
+  fontSizes: ["16px", "16px", "24px"],
   colors: {
     ...theme.colors,
     text: "#006ec8",
